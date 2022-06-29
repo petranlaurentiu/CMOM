ad61f5a6316cdfb3dadf9355acb7dd1e3f1fc8ab
Laurentiu.Petran <laurentiu.petran@BCHMRM-NMX0474.local>
2022-06-29 22:26:07 +0300

    [main]refactor the main function to accept 2 fetch API. Changed self calling function also.

--- main.js
+++ main.js
@@ -1,73 +1,83 @@
-(function fetchAPI() {
-fetch(
-"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
-)
-.then((response) => {
-if (!response.ok) {
-throw new Error("Something went wrong");
-}
-return response.json();
-})
-.then((data) => {
-console.log(data);
-const result = data
-.map((obj) => {
-return `
+function fetchAPI() {
+  let url1 =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+  const coinList = fetch(url1)
+    .then((response) => response.json())
+    .then((data) => {
+      return Promise.all(
+        data.map((coin) => {
+          return fetch("https://api.coingecko.com/api/v3/coins/" + coin.id)
+            .then((response) => response.json())
+            .then((data) => {
+              return {
+                ...coin,
+                ...data,
+              };
+            });
+        })
+      );
+    })
+    .then((data) => {
+      console.log(data);
+      let coinList = data
+        .map((coin) => {
+          return `
 <tbody>
-  <tr data-bs-toggle="collapse" href="#${obj.id}" role="button" aria-expanded="false" aria-controls="${obj.id}">
+  <tr data-bs-toggle="collapse" href="#${coin.id}" role="button" aria-expanded="false" aria-controls="${coin.id}">
     <td>
       <p>
         <a class="coin--info">
-          <img src="${obj.image}" alt="">
-          <span>${obj.name}</span>
+          <img src="${coin.image.thumb}" alt="${coin.name}">
+          <span>${coin.name}</span>
         </a>
       </p>
     </td>
-    <td><span class="coin__current--price">${obj.current_price}</span></td>
-    <td><span class="coin__high--price">${obj.high_24h} &#9650</span></td>
-    <td><span class="coin__low--price">${obj.low_24h} &#9660;</span></td>
+    <td><span class="coin__current--price">${coin.current_price}</span></td>
+    <td><span class="coin__high--price">${coin.high_24h} &#9650</span></td>
+    <td><span class="coin__low--price">${coin.low_24h} &#9660;</span></td>
   <tr>
-    <td class="collapse" id="${obj.id}">
-      <div >
-        <div class="card card-body">
-          <div class="coin--title">
-              <span>Name: ${obj.name}</span>
-              <span>Symbol: ${obj.symbol}</span>
-          </div>
+    <td class="collapse" id="${coin.id}">
+      <div class="coin__title">
+        <div>
+          <p>Name</p>
+          <p>${coin.name}</p>
         </div>
+        <div>
+          <p>Symbol </p>
+          <p>${coin.symbol}</p>
+        </div>
+        <div>
+          <p>hashing algorithm </p>
+          <p>${coin.hashing_algorithm}</p>
+        </div>
+
       </div>
     </td>
-    <td class="collapse" id="${obj.id}" >2</td>
-    <td class="collapse" id="${obj.id}" >3</td>
-    <td class="collapse" id="${obj.id}" >4</td>
-
-  </tr>
-  </tr>
-</tbody>
-
-
-
-`;
-})
-.join("");
-document.querySelector(".table").insertAdjacentHTML("afterbegin", result);
-})
-.catch((error) => {
-console.log(error);
-});
-})();
-
-/*
-<tbody>
-  <tr>
-    <td>
+    <td class="collapse" id="${coin.id}">
+      <p class="coin__description">description</p>
+      <span class="coin__description--inner">${coin.description.en}</span>
     </td>
-    </div>
-    <td><span class="coin__current--price">${obj.current_price}</span></td>
-    <td><span class="coin__high--price">${obj.high_24h} &#9650</span></td>
-    <td><span class="coin__low--price">${obj.low_24h} &#9660;</span></td>
-    <td class="more__info--innner"><button class="btn btn-sm btn-info" data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop">More Info</button></td>
+    <td class="collapse" id="${coin.id}">
+      <p class="coin__market">cap EUR</p>
+      <span>${coin.market_data.market_cap.eur}</span>
+    </td>
+    <td class="collapse" id="${coin.id}"><a href="${coin.links.homepage}" class="coin__homepage">Homepage</a>
+      <p>Genesis Date</p>
+      <span>${coin.genesis_date}</span>
+    </td>
+
+  </tr>
   </tr>
 </tbody>
-*/
+`;
+        })
+        .join(" ");
+      document
+        .querySelector(".table")
+        .insertAdjacentHTML("beforeend", coinList);
+    });
+
+  return coinList;
+}
+fetchAPI();
